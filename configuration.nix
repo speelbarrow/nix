c0094baeff4970333df415536cef67f1cfd3faa3
Noah Friedman <speelbarrow@speely.net>
2026-07-15 21:27:34 -0400

    Add more programs

--- configuration.nix
+++ configuration.nix
@@ -1,16 +1,15 @@
 { inputs, lib, pkgs, stateVersion, ... }: lib.mkMerge [
-	{
-	  environment.systemPackages = with pkgs; [
-	    nix-output-monitor
-	  ];
-	  nix.settings.experimental-features = "nix-command flakes";
-	  system = {
+  {
+    environment.systemPackages = with pkgs; [
+      nix-output-monitor
+    ];
+    nix.settings.experimental-features = "nix-command flakes";
+    system = {
       configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
-	    stateVersion = if pkgs.stdenv.hostPlatform.isDarwin then 6 else stateVersion;
+      stateVersion = if pkgs.stdenv.hostPlatform.isDarwin then 6 else stateVersion;
     };
-	}
+  }
   (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-	  security.pam.services.sudo_local.touchIdAuth = true;
+    security.pam.services.sudo_local.touchIdAuth = true;
   })
 ]
-
--- home.nix
+++ home.nix
@@ -1,3 +1,36 @@
-{ stateVersion, ... }: {
+{ pkgs, stateVersion, ... }: {
   home = { inherit stateVersion; };
+  programs = {
+    git = {
+      enable = true;
+      ignores = if pkgs.stdenv.isDarwin
+                then [".DS_Store"]
+                else [];
+      settings = {
+        core.editor = "nvim";
+        init.defaultBranch = "main";
+        push.autoSetupRemote = true;
+        user = {
+          name = "Noah Friedman";
+          email = "speelbarrow@speely.net";
+        };
+      };
+      signing = {
+        key = null;
+        signByDefault = true;
+      };
+    };
+    programs.gpg = {
+      enable = true;
+      publicKeys = [
+        { source = ./gmail.pub.asc; }
+        { source = ./speely.pub.asc; }
+      ];
+    };
+    ghostty = {
+      enable = true;
+      package = with pkgs; if stdenv.hostPlatform.isDarwin then ghostty-bin else ghostty;
+    };
+    neovim.enable = true;
+  };
 }
